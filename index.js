9e3f40382d66aa25de6ca9718c5b2beac96a7e05
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-09-07 19:01:13 +0530

    added a home page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import { configDotenv } from "dotenv";
 
@@ -7,9 +8,22 @@ configDotenv();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const __dirname = import.meta.dirname;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const data = {
+    title: "Link Shortener",
+    message: "This is a REST API for shortening long URLs.",
+  };
+  res.render("home", data);
+});
+
 app.use("/", router);
 
 app.listen(PORT, async () => {
--- routes/url.route.js
+++ routes/url.route.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
 
-import { fetchUrl, fetchAll, shrinkUrl } from "../controllers/url.controller.js";
+import {
+  fetchUrl,
+  fetchAll,
+  shrinkUrl,
+} from "../controllers/url.controller.js";
 
 const router = Router();
+
+router.get("/", (req, res) => {
+  home(req, res);
+});
 
 router.get("/all", async (req, res) => {
   await fetchAll(req, res);
@@ -12,11 +20,8 @@ router.get("/:code", async (req, res) => {
   await fetchUrl(req, res);
 });
 
-
 router.post("/shrink", async (req, res) => {
   await shrinkUrl(req, res);
 });
 
 export default router;
-
-// 1467931a
